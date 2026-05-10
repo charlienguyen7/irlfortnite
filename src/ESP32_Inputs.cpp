96ea0e6818cfa1d3ced3a057ceba75d14e27f4f9
charlie <charlien439@gmail.com>
2026-05-09 21:28:33 -0500

    Finished ESP-32 Bluetooth interfacing with trigger and joystick ADCs

--- src/ESP32_Inputs.cpp
+++ src/ESP32_Inputs.cpp
@@ -3,6 +3,7 @@ Push this code onto your ESP32
 */
 
 #include "BluetoothSerial.h"
+#define OUTPUTSIZE 3
 
 BluetoothSerial SerialBT;
 
@@ -14,21 +15,24 @@ uint8_t trigger;
 uint8_t joystickX;
 uint8_t joystickY;
 
+uint8_t output[OUTPUTSIZE]; // array containing outputs to be sent through Bluetooth
+
 void setup() {
     Serial.begin(115200);
     SerialBT.begin("ESP32_Shotgun"); // Bluetooth device name
     Serial.println("Pair with Bluetooth to Activate");
 
-    pinMode(triggerPin, INPUT); // Set trigger pin as input
-    pinMode(joystickX, INPUT);
-    pinMode(joystickY, INPUT);
+    pinMode(triggerPin, INPUT); // Set trigger pin as input (I'm using external pulldown resisitor; otherwise use INPUT_PULLDOWN)
+    pinMode(joyXPin, INPUT); // Set joystick Vx pin as input
+    pinMode(joyYPin, INPUT); // Set joystick Vy pin as input
 }
 
 void loop() {
-    trigger = digitalRead(triggerPin);
-    joystickX = (analogRead(joyXPin) >> 4); // right shift 16-bits since gamepad input is 2^8 and ADC 2^12
-    joystickY = (analogRead(joyYPin) >> 4); // right shift 16-bits since gamepad input is 2^8 and ADC 2^12
-    SerialBT.println()
+    output[0] = digitalRead(triggerPin); // read if button is being pressed    
+    output[1] = (analogRead(joyXPin) >> 4); // right shift 4-bits since gamepad input is 2^8 and ADC 2^12
+    output[2] = (analogRead(joyYPin) >> 4); // right shift 4-bits since gamepad input is 2^8 and ADC 2^12
+    SerialBT.write(output, OUTPUTSIZE); // output array containing all data elements
+    delay(20);
 }
 
 
