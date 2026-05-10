b1b8be84c1361c627a7987d8fa103c277f4c3e19
charlie <charlien439@gmail.com>
2026-05-10 00:33:47 -0500

    Wrote YOLOv26 code for human pose detection; need to interpret joint positions next to convert to joystick input

---
+++ yolo.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+# load YOLO model
+model = YOLO("yolo26n-pose.pt")
+
+while True:
+    frame = capture.read()
+
+    results = model(frame)
+    frame = results[0].plot()
+
+    cv2.imshow("Livestream", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
