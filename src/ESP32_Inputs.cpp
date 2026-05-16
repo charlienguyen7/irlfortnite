9da02ceb6a46daedce0da2c16ff0819f2b1f36f9
charlie <charlien439@gmail.com>
2026-05-15 23:38:18 -0500

    Added walking detection using Law of Cos and interfaced serial readings to controller inputs. Need to fix strafe detection

--- src/ESP32_Inputs.cpp
+++ src/ESP32_Inputs.cpp
@@ -3,17 +3,19 @@ Push this code onto your ESP32
 */
 
 #include "BluetoothSerial.h"
-#define OUTPUTSIZE 3
+#define OUTPUTSIZE 4
 
 BluetoothSerial SerialBT;
 
 const int triggerPin = 4; // GPIO pin connected to the trigger (button)
 const int joyXPin = 32; // GPIO pin connected to Vx of joystick (ADC)
 const int joyYPin = 33; // GPIO pin connected to Vy of joystick (ADC)
+const int joySWPin = 25;
 
 uint8_t trigger;
 uint8_t joystickX;
 uint8_t joystickY;
+uint8_t joystickSW;
 
 uint8_t output[OUTPUTSIZE]; // array containing outputs to be sent through Bluetooth
 
@@ -22,17 +24,19 @@ void setup() {
     SerialBT.begin("ESP32_Shotgun"); // Bluetooth device name
     Serial.println("Pair with Bluetooth to Activate");
 
-    pinMode(triggerPin, INPUT); // Set trigger pin as input (I'm using external pulldown resisitor; otherwise use INPUT_PULLDOWN)
+    pinMode(triggerPin, INPUT); // Set trigger pin as input (I'm using external pulldown resistor; otherwise use INPUT_PULLDOWN)
     pinMode(joyXPin, INPUT); // Set joystick Vx pin as input
     pinMode(joyYPin, INPUT); // Set joystick Vy pin as input
+    pinMode(joySWPin, INPUT_PULLUP);
 }
 
 void loop() {
     output[0] = digitalRead(triggerPin); // read if button is being pressed    
-    output[1] = (analogRead(joyXPin) << 3); // left shift 3-bits since gamepad input is 2^15 and ADC 2^12
-    output[2] = (analogRead(joyYPin) << 3); // left shift 3-bits since gamepad input is 2^15 and ADC 2^12
+    output[1] = (analogRead(joyXPin) >> 4); // right shift 4-bits since SerialBT.write only sends uint8_t and ADC 2^12
+    output[2] = (analogRead(joyYPin) >> 4); // right shift 4-bits since SerialBT.write only sends uint8_t and ADC 2^12
+    output[3] = digitalRead(joySWPin); // read if joystick button is pressed
     SerialBT.write(output, OUTPUTSIZE); // output array containing all data elements
-    delay(20);
+    delay(10);
 }
 
 
--- yolo.py
+++ yolo.py
@@ -1,13 +1,11 @@
 from ultralytics import YOLO
+from ultralytics import solutions
 import vgamepad as vg
 import cv2
+import threading
 import time
-
-# initalize XBox 360 gamepad
-gamepad = vg.VX360Gamepad()
-
-buttonTime = 0.2
-buttonPressed = 0
+import math
+import serial
 
 # camera dimensions
 CAM_WIDTH = 640
@@ -18,15 +16,48 @@ fps_start = 0
 fps_end = 0
 
 # global variable declaring current state (STANDING, RUNNING, JUMPING, CROUCHING)
-state = "STANDING"
-STATE_DELAY = 0.5 # Delay between each state meant to combat constant switching
-MOVEMENT_THRESHOLD = 0.35 # Difference between hips and knees to constitute as "MOVEMENT"
-RUNNING_THRESHOLD = 0.7 # Time in seconds between left knee and right knee reaching MOVEMENT_THRESHOLD to constitute as "RUNNING"
-JUMPING_THRESHOLD = 0.45 # Normalized y-value of the knees to constitute as "JUMPING"
+lateralState = "STANDING"
+verticalState = "STANDING"
 
-leftKneeTime = 0
-rightKneeTime = 0
-lastActivityTime = time.time()
+# value from -32768 to 32767
+strafeDirection = 0
+
+RUNNING_THRESHOLD = 0.3 # Time in seconds between left knee and right knee reaching MOVEMENT_THRESHOLD to constitute as "RUNNING"
+RUNNING_THRESHOLD_MIN = 0.1 # Minimum time discrepancy between left leg and right leg lifting; meant to prevent crouching from registering as "RUNNING"
+RUNNING_ANGLE = 170 # Angle between hips, knees, and ankle to be considered "RUNNING"
+JUMPING_THRESHOLD = 0.6 # Normalized y-value of the knees to constitute as "JUMPING"
+
+# global default controller values
+trigger = 0 # default is 0 because active-high (positive) logic
+joyVX = 0x74
+joyVY = 0x7b
+joySW = 0
+
+normalizedQuad = 0
+normalizedHip = 0
+
+# global variable for camera frame and joint angles
+frame = None
+
+leftHip = 0
+leftKnee = 0
+
+leftAngle = 0
+rightAngle = 0
+
+# initalize XBox 360 gamepad
+gamepad = vg.VX360Gamepad()
+
+# set up camera stream
+capture = cv2.VideoCapture(1)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
+
+# load YOLO model
+model = YOLO("yolo26m-pose.pt")
+
+# set up to read from serial COM7 (Bluetooth)
+ser = serial.Serial('COM7', 115200, bytesize=8, parity='N', stopbits=1)
 
 # function to print frame rate of camera stream
 def printFPS():
@@ -37,87 +68,165 @@ def printFPS():
     fps_text = "FPS: {:.2f}".format(fps)
     cv2.putText(frame, fps_text, (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
 
-capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
+# function to read data from COM7
+def serialRead():
+    global trigger, joyVX, joyVY, joySW
+    serialData = None
+    while True:
+        serialData = ser.read(4)
+        trigger = serialData[0]
+        joyVX = (serialData[1] << 8) - 32768 # gamepad joystick takes in values from -32768 and 32767; serialData[1] is a uint8_t
+        joyVY = (serialData[2] << 8) - 32768 # gamepad joystick takes in values from -32768 and 32767; serialData[1] is a uint8_t
+        joySW = not serialData[3]
+        print(trigger," ",joyVX," ",joyVY," ",joySW)
 
-# load YOLO model
-model = YOLO("yolo26n-pose.pt")
-
-while True:
-    ret,frame = capture.read()
-
-    results = model(frame)
-    frame = results[0].plot()
-
-    # extract normalized keypoints from results
-    keypoints = results[0].keypoints.xyn
-
-    # you need this because if there is no person in frame, code will crash since 
-    # size of first index of keypoints (which represents idx of person) is 0
-    try:
-        leftKnee = keypoints[0][13][1]
-        rightKnee = keypoints[0][14][1]
-
-        leftHip = keypoints[0][11][1]
-        rightHip = keypoints[0][12][1]
-    except IndexError:
-        continue
-    
-    leftDisplacement = abs(leftKnee - leftHip)
-    rightDisplacement = abs(rightKnee - rightHip)
-
-    if leftDisplacement < MOVEMENT_THRESHOLD:
-        leftKneeTime = time.time()
-    if rightDisplacement < MOVEMENT_THRESHOLD:
-        rightKneeTime = time.time()
-
-    if time.time() - leftKneeTime > RUNNING_THRESHOLD:
-        leftKneeTime = 0
-    if time.time() - rightKneeTime > RUNNING_THRESHOLD:
-        rightKneeTime = 0
-    
-    if (leftKneeTime != 0 and rightKneeTime != 0) and (abs(leftKneeTime - rightKneeTime) < RUNNING_THRESHOLD):
-        state = "RUNNING"
-        lastActivityTime = time.time()
-    
-    if leftKnee < JUMPING_THRESHOLD and rightKnee < JUMPING_THRESHOLD:
-        state = "JUMPING"
-        lastActivityTime = time.time()
-
-    if time.time() - lastActivityTime >= STATE_DELAY:
-        state = "STANDING"
-    
-    cv2.putText(frame, state, (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-
-    leftKneeText = "Left Knee: {:.2f}".format(leftKnee)
-    rightKneeText = "Right Knee: {:.2f}".format(rightKnee)
-    leftHipText = "Left Hip: {:.2f}".format(leftHip)
-    rightHipText = "Right Hip: {:.2f}".format(rightHip)
-    # cv2.putText(frame, leftHipText, (0,75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-    # cv2.putText(frame, rightHipText, (0,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-
+# function to display frame onto cv2
+def yoloDisplay():
+    global frame, leftAngle, rightAngle, lateralState, leftHip, leftKnee
+    cv2.putText(frame, "{:.2f}".format(leftAngle), (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(frame, "{:.2f}".format(rightAngle), (0,75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(frame, lateralState, (0,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    # cv2.putText(frame, "Left Hip: {:.2f}".format(leftHip[0]), (0,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    # cv2.putText(frame, "Left Knee: {:.2f}".format(leftKnee[0]), (0,325), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(frame, "NZD Hip: {:.2f}".format(normalizedHip), (0,325), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(frame, "NZD Quad: {:.2f}".format(normalizedQuad), (0,350), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
     printFPS()
     cv2.imshow("Livestream", frame)
 
-    if state == "JUMPING":
-        gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
-        gamepad.update()
-        buttonPressed = time.time()
-    elif state == "STANDING":
-        gamepad.left_joystick(0,0)
-        gamepad.update()
-    elif state == "RUNNING":
-        gamepad.left_joystick(0, 32767)
-        gamepad.update()
+# function that contains YOLO prediction model
+def yoloPredict():
+    global frame, state, leftHip, leftKnee, leftAngle, rightAngle, normalizedQuad, normalizedHip
+    while True:
+        ret, frame = capture.read()
+
+        results = model.predict(frame, device=0, verbose=False, max_det=1)
+        frame = results[0].plot()
+
+        # extract normalized keypoints from results
+        keypoints = results[0].keypoints.xyn
+
+        # you need this because if there is no person in frame, code will crash since 
+        # size of first index of keypoints (which represents idx of person) is 0
+        try:
+            # keypoints for both knees
+            leftKnee = keypoints[0][13]
+            rightKnee = keypoints[0][14]
+
+            # keypoints for both hips
+            leftHip = keypoints[0][11]
+            rightHip = keypoints[0][12]
+            
+            # keypoints for both ankles
+            leftAnkle = keypoints[0][15]
+            rightAnkle = keypoints[0][16]
+
+            # triangle side lengths of left side
+            l1 = math.pow(leftKnee[0]-leftHip[0], 2) + math.pow(leftKnee[1]-leftHip[1], 2)
+            l2 = math.pow(leftAnkle[0]-leftKnee[0], 2) + math.pow(leftAnkle[1]-leftKnee[1], 2)
+            l3 = math.pow(leftHip[0]-leftAnkle[0], 2) + math.pow(leftHip[1]-leftAnkle[1], 2)
+
+            # triangle side lengths of right side
+            r1 = math.pow(rightKnee[0]-rightHip[0], 2) + math.pow(rightKnee[1]-rightHip[1], 2)
+            r2 = math.pow(rightAnkle[0]-rightKnee[0], 2) + math.pow(rightAnkle[1]-rightKnee[1], 2)
+            r3 = math.pow(rightHip[0]-rightAnkle[0], 2) + math.pow(rightHip[1]-rightAnkle[1], 2)
+
+            # length of hip
+            h1 = math.pow(rightHip[0]-leftHip[0], 2) + math.pow(rightHip[1]-leftHip[1], 2)
+
+            normalizedQuad = (leftKnee[0] - leftHip[0])/l1
+            normalizedHip = h1/l1
+
+            # Law of Cosine to calculate angle between hip, knee, and ankle
+            try:
+                leftAngle = math.acos((l1 + l2 - l3)/(2*math.sqrt(l1)*math.sqrt(l2))) * 180 / 3.141592653
+                rightAngle = math.acos((r1 + r2 - r3)/(2*math.sqrt(r1)*math.sqrt(r2))) * 180 / 3.141592653
+            except ValueError:
+                pass
     
-    if time.time() - buttonPressed > buttonTime:
-        gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
-        gamepad.update()
-        buttonPressed = 0
+        except IndexError:
+            continue
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        yoloDisplay()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            running = False
+            break
+
+# function that checks YOLO pose keypoints and determines state of player (i.e. jumping, running)
+def movementState():
+    global lateralState, verticalState, leftHip, leftKnee, strafeDirection, normalizedQuad, normalizedHip, leftHip, rightHip
+    leftLegTime = 0
+    rightLegTime = 0
+    timeOfActivity = 0
+    lastStrafeTime = 0
+    while True:
+
+        # denote the time at which legs bent
+        if leftAngle < RUNNING_ANGLE:
+            leftLegTime = time.time() 
+        if rightAngle < RUNNING_ANGLE:
+            rightLegTime = time.time()
+
+        # if legs bent a certain time between each other, then constitute as "RUNNING"
+        runningTime = abs(leftLegTime - rightLegTime)
+        if RUNNING_THRESHOLD_MIN < runningTime < RUNNING_THRESHOLD:
+            lateralState = "RUNNING"
+            timeOfActivity = time.time()
+            leftLegTime = timeOfActivity
+            rightLegTime = timeOfActivity
+
+            if normalizedQuad > 1:
+                strafeDirection = -20000 # strafe left
+                
+            elif normalizedQuad < -1:
+                if normalizedHip < 0.1:
+                    strafeDirection = 20000 # strafe right
+                    
+        if time.time() - timeOfActivity > RUNNING_THRESHOLD:
+            lateralState = "STANDING"
+            
+        time.sleep(0.01)
+
+
+# function that feeds gamepad input
+
+def gamepadInput():
+    global gamepad, lateralState, verticalState, strafeDirection, trigger, joyVX, joyVY, joySW
+    while True:
+        gamepad.right_joystick(joyVX, joyVY)
+        
+        if verticalState == "JUMPING":
+            gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+        elif lateralState == "STANDING":
+            gamepad.left_joystick(0,0)
+        elif lateralState == "RUNNING":
+            gamepad.left_joystick(strafeDirection, 32767)
+
+        # if pressed trigger, shoot gun
+        if trigger:
+            gamepad.right_trigger(255)
+        else:
+            gamepad.right_trigger(0)
+
+        # if pressed joystick button, reload shotgun
+        if joySW:
+            gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
+        else:
+            gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_X)
+            
+        gamepad.update()
+        
+yoloThread = threading.Thread(target=yoloPredict, daemon=True)
+serialThread = threading.Thread(target=serialRead, daemon=True)
+movementThread = threading.Thread(target=movementState, daemon=True)
+gamepadThread = threading.Thread(target=gamepadInput, daemon=True)
+
+yoloThread.start()
+serialThread.start()
+movementThread.start()
+gamepadThread.start()
+
+yoloThread.join()
 
 capture.release()
 cv2.destroyAllWindows()
