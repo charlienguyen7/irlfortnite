65efd64d1f1a180f29d48c7cc3166eb27abf596a
charlie <charlien439@gmail.com>
2026-05-17 23:07:50 -0500

    Strafing logic done; works but janky

--- yolo.py
+++ yolo.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 from ultralytics import solutions
+from collections import deque
 import vgamepad as vg
 import cv2
 import threading
@@ -21,8 +22,9 @@ verticalState = "STANDING"
 
 # value from -32768 to 32767
 strafeDirection = 0
+direction = "STRAIGHT"
 
-RUNNING_THRESHOLD = 0.3 # Time in seconds between left knee and right knee reaching MOVEMENT_THRESHOLD to constitute as "RUNNING"
+RUNNING_THRESHOLD = 0.8 # Time in seconds between left knee and right knee reaching MOVEMENT_THRESHOLD to constitute as "RUNNING"
 RUNNING_THRESHOLD_MIN = 0.1 # Minimum time discrepancy between left leg and right leg lifting; meant to prevent crouching from registering as "RUNNING"
 RUNNING_ANGLE = 170 # Angle between hips, knees, and ankle to be considered "RUNNING"
 JUMPING_THRESHOLD = 0.6 # Normalized y-value of the knees to constitute as "JUMPING"
@@ -34,7 +36,7 @@ joyVY = 0x7b
 joySW = 0
 
 normalizedQuad = 0
-normalizedHip = 0
+shoulderLen = 0
 
 # global variable for camera frame and joint angles
 frame = None
@@ -78,24 +80,24 @@ def serialRead():
         joyVX = (serialData[1] << 8) - 32768 # gamepad joystick takes in values from -32768 and 32767; serialData[1] is a uint8_t
         joyVY = (serialData[2] << 8) - 32768 # gamepad joystick takes in values from -32768 and 32767; serialData[1] is a uint8_t
         joySW = not serialData[3]
-        print(trigger," ",joyVX," ",joyVY," ",joySW)
+        # print(trigger," ",joyVX," ",joyVY," ",joySW)
 
 # function to display frame onto cv2
 def yoloDisplay():
-    global frame, leftAngle, rightAngle, lateralState, leftHip, leftKnee
-    cv2.putText(frame, "{:.2f}".format(leftAngle), (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-    cv2.putText(frame, "{:.2f}".format(rightAngle), (0,75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    global frame, leftAngle, rightAngle, lateralState, leftHip, leftKnee, normalizedQuad, shoulderLen, direction
+    cv2.putText(frame, "Left: {:.2f}".format(leftAngle), (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(frame, "Right: {:.2f}".format(rightAngle), (0,75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
     cv2.putText(frame, lateralState, (0,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-    # cv2.putText(frame, "Left Hip: {:.2f}".format(leftHip[0]), (0,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(frame, direction, (0,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
     # cv2.putText(frame, "Left Knee: {:.2f}".format(leftKnee[0]), (0,325), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-    cv2.putText(frame, "NZD Hip: {:.2f}".format(normalizedHip), (0,325), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    cv2.putText(frame, "Shoulder Len: {:.2f}".format(shoulderLen), (0,325), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
     cv2.putText(frame, "NZD Quad: {:.2f}".format(normalizedQuad), (0,350), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
     printFPS()
     cv2.imshow("Livestream", frame)
 
 # function that contains YOLO prediction model
 def yoloPredict():
-    global frame, state, leftHip, leftKnee, leftAngle, rightAngle, normalizedQuad, normalizedHip
+    global frame, state, leftHip, leftKnee, leftAngle, rightAngle, normalizedQuad, shoulderLen
     while True:
         ret, frame = capture.read()
 
@@ -120,6 +122,9 @@ def yoloPredict():
             leftAnkle = keypoints[0][15]
             rightAnkle = keypoints[0][16]
 
+            leftShoulder = keypoints[0][5]
+            rightShoulder = keypoints[0][6]
+
             # triangle side lengths of left side
             l1 = math.pow(leftKnee[0]-leftHip[0], 2) + math.pow(leftKnee[1]-leftHip[1], 2)
             l2 = math.pow(leftAnkle[0]-leftKnee[0], 2) + math.pow(leftAnkle[1]-leftKnee[1], 2)
@@ -130,11 +135,14 @@ def yoloPredict():
             r2 = math.pow(rightAnkle[0]-rightKnee[0], 2) + math.pow(rightAnkle[1]-rightKnee[1], 2)
             r3 = math.pow(rightHip[0]-rightAnkle[0], 2) + math.pow(rightHip[1]-rightAnkle[1], 2)
 
-            # length of hip
-            h1 = math.pow(rightHip[0]-leftHip[0], 2) + math.pow(rightHip[1]-leftHip[1], 2)
+            # length of torso
+            mid_shoulder_y = (leftShoulder[1] + rightShoulder[1]) / 2
+            mid_hip_y = (leftHip[1] + rightHip[1]) / 2
+            torso_height = abs(mid_shoulder_y - mid_hip_y)
 
+            # length of shoulders
+            shoulderLen = math.sqrt((math.pow(leftShoulder[0]-rightShoulder[0], 2) + math.pow(leftShoulder[1]-rightShoulder[1],2)) / torso_height) * 100
             normalizedQuad = (leftKnee[0] - leftHip[0])/l1
-            normalizedHip = h1/l1
 
             # Law of Cosine to calculate angle between hip, knee, and ankle
             try:
@@ -149,42 +157,51 @@ def yoloPredict():
         yoloDisplay()
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            running = False
             break
 
 # function that checks YOLO pose keypoints and determines state of player (i.e. jumping, running)
 def movementState():
-    global lateralState, verticalState, leftHip, leftKnee, strafeDirection, normalizedQuad, normalizedHip, leftHip, rightHip
+    global lateralState, verticalState, leftHip, leftKnee, strafeDirection, normalizedQuad, shoulderLen, direction
     leftLegTime = 0
     rightLegTime = 0
     timeOfActivity = 0
-    lastStrafeTime = 0
+    leftStepSeen = False
+    rightStepSeen = False
     while True:
 
-        # denote the time at which legs bent
-        if leftAngle < RUNNING_ANGLE:
-            leftLegTime = time.time() 
-        if rightAngle < RUNNING_ANGLE:
+        # left step event
+        if leftAngle < RUNNING_ANGLE and not leftStepSeen:
+            leftLegTime = time.time()
+            leftStepSeen = True
+
+        # right step event
+        if rightAngle < RUNNING_ANGLE and not rightStepSeen:
             rightLegTime = time.time()
+            rightStepSeen = True
 
-        # if legs bent a certain time between each other, then constitute as "RUNNING"
-        runningTime = abs(leftLegTime - rightLegTime)
-        if RUNNING_THRESHOLD_MIN < runningTime < RUNNING_THRESHOLD:
-            lateralState = "RUNNING"
-            timeOfActivity = time.time()
-            leftLegTime = timeOfActivity
-            rightLegTime = timeOfActivity
-
+        if leftStepSeen and rightStepSeen:
+            runningTime = abs(leftLegTime - rightLegTime)
+            print("BOTH DETECTED dt:", runningTime)
+            if RUNNING_THRESHOLD_MIN < runningTime < RUNNING_THRESHOLD:
+                lateralState = "RUNNING"
+                timeOfActivity = time.time()
+            leftStepSeen = False
+            rightStepSeen = False
+        
+        if shoulderLen < 7:
             if normalizedQuad > 1:
-                strafeDirection = -20000 # strafe left
-                
-            elif normalizedQuad < -1:
-                if normalizedHip < 0.1:
-                    strafeDirection = 20000 # strafe right
-                    
+                strafeDirection = -25000
+                direction = "LEFT"
+            if normalizedQuad < -1:
+                strafeDirection = 25000
+                direction = "RIGHT"
+        else:
+            strafeDirection = 0
+            direction = "STRAIGHT"
+        
         if time.time() - timeOfActivity > RUNNING_THRESHOLD:
             lateralState = "STANDING"
-            
+
         time.sleep(0.01)
 
 
